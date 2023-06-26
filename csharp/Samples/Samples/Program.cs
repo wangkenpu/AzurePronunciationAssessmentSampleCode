fab59f32c53da3187998a419ce93cdedc1c5357d
Ke WANG <wangkenpu@outlook.com>
2023-06-26 16:18:58 +0800

    style: re-order packages

--- csharp/Samples/Samples/Program.cs
+++ csharp/Samples/Samples/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 using speechsdk = Microsoft.CognitiveServices.Speech;
-using Newtonsoft.Json;
-using System.IO;
+
 
 namespace Samples
 {
